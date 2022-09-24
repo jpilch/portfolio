62f28346fce4bec9c6c6bb388231091169fe82dc
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-09-24 12:59:05 +0200

    make theme icon clickable and update links

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -38,7 +38,7 @@ export default function Footer() {
                     <ul className={styles.list}>
                         <li>
                             <a
-                                href="https://www.linkedin.com/in/jan-pilch-202a30249/"
+                                href="https://drive.google.com/file/d/1bgxGmL_Kb_FYjqD9kgYfzoYKg1XHBWr3/view?usp=sharing"
                                 target="_blank"
                             >
                                 &gt; CCNA R&amp;S: Introduction to Networks
@@ -46,7 +46,7 @@ export default function Footer() {
                         </li>
                         <li>
                             <a
-                                href="https://github.com/jpilch"
+                                href="https://drive.google.com/file/d/1csMh8vGiRNgR6w8w1EkeE0tz8NYQt699/view?usp=sharing"
                                 target="_blank"
                             >
                                 &gt; CCNAv7: Switching, Routing, and Wireless Essentials
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,8 +4,11 @@ import cn from "classnames";
 
 import Brightness6Icon from '@mui/icons-material/Brightness6';
 
+import { useRef } from "react";
 
 export default function Header() {
+    const themeIconRef = useRef<SVGSVGElement>(null);
+
     return (
         <header className={cn(
             utilStyles["flex-container"],
@@ -20,10 +23,31 @@ export default function Header() {
                     <li>Skills</li>
                     <li>Projects</li>
                     <li>Contact</li>
-                    <li><Brightness6Icon
-                        fontSize="small"
-                        className="icon"
-                    /></li>
+                    <li>
+                        <a
+                            href="https://drive.google.com/file/d/1GZjV3zYTT-atfjGKYtjkFyEeZH9ClXoH/view?usp=sharing"
+                            target={"_blank"}
+                        >
+                            Resume
+                        </a>
+                    </li>
+                    <li
+                        className={utilStyles["flex-container"]}
+                        onClick={e => {
+                            const target = e.target as HTMLElement;
+                            if (!target.closest("svg")) return false;
+                            themeIconRef.current?.classList.toggle("toggled");
+                        }}
+                    >
+                        <Brightness6Icon
+                            fontSize="small"
+                            className={cn(
+                                "icon",
+                                "theme-icon"
+                            )}
+                            ref={themeIconRef}
+                        />
+                    </li>
                 </ul>
             </nav>
         </header >
